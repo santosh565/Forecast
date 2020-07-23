1a8c84c8a17578bcd1276eed07c4c939b8e40068
Santosh Bhattarai <hsotnas76@gmail.com>
2020-07-23 17:24:49 +0545

    show corresponding day of week in forecast card

--- lib/ui/forecast_card.dart
+++ lib/ui/forecast_card.dart
@@ -2,8 +2,18 @@
 
 import 'package:flutter/material.dart';
 import 'package:weather_forecast/model/weather_forecast_model.dart';
+import 'package:weather_forecast/util/weather_forecast_util.dart';
 
 Widget forecastCard(AsyncSnapshot<WeatherForecastModel>snapshot,int index){
-  
+  var jsonData = snapshot.data.list;
+  DateTime date = DateTime.fromMillisecondsSinceEpoch(jsonData[index].dt*1000);
+  var fullDate = Util.getFormattedDate(date);
+  var dayOfWeek = fullDate.split(',')[0];// returns day of week
+
+  return Column(
+    children: <Widget>[
+      Text(dayOfWeek),
+    ],
+  );
 
 }
--- lib/util/weather_forecast_util.dart
+++ lib/util/weather_forecast_util.dart
@@ -4,7 +4,7 @@ class Util{
 
     static String appId='ed60fcfbd110ee65c7150605ea8aceea';
     static String getFormattedDate(DateTime dateTime){
-      return DateFormat('EEE, MMM d, y').format(dateTime);
+      return DateFormat('EEEE, MMM d, y').format(dateTime);
 
     }
   }
